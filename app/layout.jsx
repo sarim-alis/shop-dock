56d9282f83e78cdd918c05b1be3ef04342cfa511
sarim-alis <sarimslayerali786@gmail.com>
2025-11-16 15:20:38 +0500

    feat/Build setup layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -13,7 +13,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
     return (
-      <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+      <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY} dynamic>
         <html lang="en">
             <body className={`${outfit.className} antialiased`}>
                 <StoreProvider>
