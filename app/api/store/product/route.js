8f9848e0e6a4b633ade9e6dff55754f6310ad9de
sarim-alis <sarimslayerali786@gmail.com>
2025-11-16 21:17:27 +0500

    feat/Auth seller setup

---
+++ app/api/store/product/route.js
@@ -0,0 +1,5 @@
+import imagekit from "../../../../configs/imageKit";
+import prisma from "../../../../lib/prisma";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
