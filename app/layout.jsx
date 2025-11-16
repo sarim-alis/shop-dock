96eb6ab7bf63037f0ffc932280336c7d3f59ab11
sarim-alis <sarimslayerali786@gmail.com>
2025-11-16 15:12:14 +0500

    feat/Use client

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,3 @@
-'use client';
 import { Outfit } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 import StoreProvider from "@/app/StoreProvider";
@@ -7,14 +6,15 @@ import { ClerkProvider } from "@clerk/nextjs";
 
 const outfit = Outfit({ subsets: ["latin"], weight: ["400", "500", "600"] });
 
+export const metadata = {
+    title: "ShopDock - Shop smarter",
+    description: "ShopDock - Shop smarter",
+};
+
 export default function RootLayout({ children }) {
     return (
       <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
         <html lang="en">
-            <head>
-                <title>ShopDock - Shop smarter</title>
-                <meta name="description" content="ShopDock - Shop smarter" />
-            </head>
             <body className={`${outfit.className} antialiased`}>
                 <StoreProvider>
                     <Toaster />
