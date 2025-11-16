e86bd74754320e6e69b76212124de449ce013414
sarim-alis <sarimslayerali786@gmail.com>
2025-11-17 00:11:20 +0500

    feat/data setup

---
+++ app/api/store/data/route.js
@@ -0,0 +1,10 @@
+import prisma from "../../../../lib/prisma";
+import authSeller from "../../../../middlewares/authSeller"
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+
+// Get store info & store products.
+export async function GET (request) {
+    
+}
