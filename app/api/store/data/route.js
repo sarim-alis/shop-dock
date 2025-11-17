f5ce34079652028d6ea793477b4690f6dcbf0814
sarim-alis <sarimslayerali786@gmail.com>
2025-11-17 17:43:33 +0500

    feat/Data setup

--- app/api/store/data/route.js
+++ app/api/store/data/route.js
@@ -1,10 +1,30 @@
 import prisma from "../../../../lib/prisma";
-import authSeller from "../../../../middlewares/authSeller"
-import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
-
 
 // Get store info & store products.
 export async function GET (request) {
-    
+    try {
+        // Get store username from query params.
+        const { searchParams } = new URL(request.url)
+        const username = searchParams.get('username').toLowerCase();
+
+        if (!username) {
+            return NextResponse.json({ error: "Username is required" }, { status: 400 })
+        }
+
+        // Get store info and inStock products with ratings.
+        const store = await prisma.store.findUnique({
+            where:   { username, isActive: true },
+            include: { Product: { include: { rating: true }}}
+        })
+
+        if (!store) {
+            return NextResponse.json({error: "Store not found"}, { status: 400 })
+        }
+        
+        return NextResponse.json({store})
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ error: error.code || error.message }, { status:400 })
+    }
 }
