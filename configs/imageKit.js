28f595dcf29ef365844adf52e6462e6716026500
sarim-alis <sarimslayerali786@gmail.com>
2025-11-17 16:56:09 +0500

    feat/Image kit setup

--- configs/imageKit.js
+++ configs/imageKit.js
@@ -1,8 +1,8 @@
 import ImageKit from "imagekit";
 
 var imagekit = new ImageKit({
-    publickey :   process.env.IMAGEKIT_PUBLIC_KEY,
-    privatekey :  process.env.IMAGEKIT_PRIVATE_KEY,
+    publicKey :   process.env.IMAGEKIT_PUBLIC_KEY,
+    privateKey :  process.env.IMAGEKIT_PRIVATE_KEY,
     urlEndpoint : process.env.IMAGEKIT_URL_ENDPOINT
 });
 
