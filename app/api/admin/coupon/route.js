45352dfb51897c2b72b1414b91eed5a8da75570a
sarim-alis <sarimslayerali786@gmail.com>
2025-11-19 00:51:12 +0500

    feat/Coupon api

--- app/api/admin/coupon/route.js
+++ app/api/admin/coupon/route.js
@@ -26,3 +26,41 @@ export async function POST(request) {
         return NextResponse.json({ error: error.code || error.message }, { status:400 })
     }
 }
+
+// Delete coupon  /api/coupon?id=couponId
+export async function DELETE(request) {
+    try {
+        const { userId } = getAuth()
+        const isAdmin    = await authAdmin(userId)
+
+        if (!isAdmin) {
+            return NextResponse.json({ error: "Not Authorized" }, { status: 401 });
+        }
+
+        const { searchParams } = request.nextUrl;
+        const code = searchParams.get('code');
+
+        await prisma.coupon.delete({ where: { code } })
+        return NextResponse.json({ message: "Coupon deleted successfully" })
+    } catch (error) {
+        console.error(error)
+        return NextResponse.json({ error: error.code || error.message }, { status:400 })
+    }
+}
+
+// Get all coupons.
+export async function GET(request) {
+    try {
+        const { userId } = getAuth()
+        const isAdmin    = await authAdmin(userId)
+
+        if (!isAdmin) {
+            return NextResponse.json({ error: "Not Authorized" }, { status: 401 });
+        }
+        const coupons = await prisma.coupon.findMany({})
+        return NextResponse.json({ coupons })
+    } catch (error) {
+        console.error(error)
+        return NextResponse.json({ error: error.code || error.message }, { status:400 })
+    }
+}
