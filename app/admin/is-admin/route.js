0f2fafe822a734d3560486ac9146951cacfc8e80
sarim-alis <sarimslayerali786@gmail.com>
2025-11-18 12:51:18 +0500

    feat/Is admin

---
+++ app/admin/is-admin/route.js
@@ -0,0 +1,21 @@
+import authAdmin from "../../../middlewares/authAdmin.js";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+
+// Auth admin.
+export async function GET (request) {
+    try {
+        const { userId } = getAuth(request)
+        const isAdmin    = await authAdmin(userId)
+
+        if (!isAdmin) {
+            return NextResponse.json({ error: "Not authorized" }, { status: 401 })
+        }
+
+        return NextResponse.json({isAdmin})
+    } catch (error) {
+        console.error(error)
+        return NextResponse.json({ error: error.code || error.message }, { status:400 })
+    }
+}
