228529bb33223fde5769638b1bb76a6e7c05073c
sarim-alis <sarimslayerali786@gmail.com>
2025-11-19 15:40:30 +0500

    feat/Orders get api

--- app/api/store/order/route.js
+++ app/api/store/order/route.js
@@ -27,3 +27,26 @@ export async function POST (request) {
         return NextResponse.json({ error: error.code || error.message }, { status:400 })
     }
 }
+
+// Get all orders for seller.
+export async function GET (request) {
+  try {
+      const { userId } = getAuth(request)
+      const storeId    = await authSeller(userId)
+
+      if (!storeId) {
+        return NextResponse.json({ error: "Not authorized" }, { status: 401 });
+      }
+
+      const orders = await prisma.order.findMany({
+        where: { storeId },
+        include: { user: true, address: true, orderItems: {include: {product: true}}},
+        orderBy: { createdAt: 'desc' }
+      })
+
+      return NextResponse.json({orders})
+  } catch (error) {
+      console.error(error);
+      return NextResponse.json({ error: error.code || error.message }, { status:400 })
+  }
+}
