051e1be940773516dae901a8f7ca456ee92047c9
sarim-alis <sarimslayerali786@gmail.com>
2025-11-20 01:00:45 +0500

    feat/Address, product, cart route

--- app/api/cart/route.js
+++ app/api/cart/route.js
@@ -1,4 +1,38 @@
 import prisma from "../../../lib/prisma.js";
-import authAdmin from "../../..//middlewares/authAdmin.js";
 import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
+
+// Update user cart.
+export async function POST (request) {
+    try {
+        const { userId } = getAuth(request)
+        const { cart   } = await request.json()
+
+        // Save cart to user object.
+        await prisma.user.update({
+            where: { id: userId },
+            data:  { cart: cart}
+        })
+
+        return NextResponse.json({ message: 'Cart updated' })
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ error: 'Internal server error'}, { status: 500 });
+    }
+}
+
+// Get user cart.
+export async function GET (request) {
+    try {
+        const { userId } = getAuth(request)
+
+        const user = await prisma.user.findUnique({
+            where: { id: userId }
+        })
+
+        return NextResponse.json({ cart: user.cart })
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ error: error.message }, { status: 400 });
+    }
+}
--- app/api/products/route.js
+++ app/api/products/route.js
@@ -3,3 +3,27 @@ import authAdmin from "../../..//middlewares/authAdmin.js";
 import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
+export async function GET (request) {
+    try {
+        let products = await prisma.product.findMany({
+            where: { inStock: true },
+            include: {
+                rating: {
+                    select: {
+                        createdAt: true, rating: true, review: true,
+                        user: { select: {name: true, image: true}}
+                    }
+                },
+                store: true,
+            },
+            orderBy: { createdAt: 'desc' }
+        })
+
+        // Remove products with store inActive false.
+        products = products.filter(product => product.store.isActive)
+        return NextResponse.json({products})
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ error: 'Internal server error'}, { status: 500 });
+    }
+}
