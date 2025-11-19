4988860f52f963b25d98998826dae7b23b6888a4
sarim-alis <sarimslayerali786@gmail.com>
2025-11-19 15:53:46 +0500

    feat/Products setup

---
+++ app/api/products/route.js
@@ -0,0 +1,5 @@
+import prisma from "../../../lib/prisma.js";
+import authAdmin from "../../..//middlewares/authAdmin.js";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
