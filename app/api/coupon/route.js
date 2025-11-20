f3379dc3f4e45f71d47edc4088f4bfce0b66fc86
sarim-alis <sarimslayerali786@gmail.com>
2025-11-20 17:18:29 +0500

    feat/Coupon api setup

---
+++ app/api/coupon/route.js
@@ -0,0 +1,6 @@
+import prisma from "../../../lib/prisma.js";
+import authAdmin from "../../..//middlewares/authAdmin.js";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+// Update user cart.
